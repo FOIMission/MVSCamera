d10cf450e4b3ffe434de56c5c8548792694e9ca0
xiaoyu <MegatronPrime113@163.com>
2026-01-14 15:15:47 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "mvscamera.h"
+#include "mydismeasuring_pen.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MVSCamera MVSCamera_w;
+    MVSCamera_w.show();
+    Mydismeasuring_pen Mydismeasuring_pen_w;
+    Mydismeasuring_pen_w.show();
+    return a.exec();
+}
---
+++ mvscamera.h
@@ -0,0 +1,48 @@
+#ifndef MVSCAMERA_H
+#define MVSCAMERA_H
+
+#include <QMainWindow>
+#include "MvCameraControl.h"
+#include <QImage>
+#include <QPixmap>
+#include <QDateTime>
+#include <QMessageBox>
+#include <QDir>
+#include <QDialog>
+#include <QDebug>
+#include <QMessageBox>
+#include <QErrorMessage>
+#include <QApplication>
+#include <QPainter>
+#include <QScreen>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MVSCamera; }
+QT_END_NAMESPACE
+
+class MVSCamera : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MVSCamera(QWidget *parent = nullptr);
+    ~MVSCamera();
+
+    int nRet = MV_OK;
+    void * handle=NULL;
+
+protected:
+        Ui::MVSCamera *ui;
+
+private slots:
+    void on_Preview_clicked();
+    void on_Stop_clicked();
+    void on_Capture_clicked();
+
+
+private:
+    void showImage(QImage showImage);
+    static void __stdcall ImageCallBack (unsigned char *pData, MV_FRAME_OUT_INFO_EX *pFrameInfo, void *pUser);
+    void Initialize();
+    void initWindow();
+};
+#endif // MVSCAMERA_H
